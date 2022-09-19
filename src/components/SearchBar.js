42e92a599dc59983d69320b9ff0ce943842b5ec8
fadhilrobbani <rabbanifadhillailham@gmail.com>
2022-09-19 11:06:36 +0700

    finish

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
 import { FaSearch } from 'react-icons/fa';
 import { ImCross } from 'react-icons/im';
